a70523f69720db2ff7e0c7db19b9b1ad7e24e87f
luv4code-lfc <luv4code.lc@gmail.com>
2021-12-01 12:33:07 +0530

    saving new beer using rest template

--- src/test/java/com/luv4code/lfcbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/com/luv4code/lfcbreweryclient/web/client/BreweryClientTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,5 +20,13 @@ class BreweryClientTest {
     void getBeerById() {
         BeerDto beerDto = breweryClient.getBeerById(UUID.randomUUID());
         assertNotNull(beerDto);
+    }
+
+    @Test
+    void testSaveNewBeer() {
+        BeerDto beerDto = BeerDto.builder().beerName("Pale Ale").build();
+        URI uri = breweryClient.saveNewBeer(beerDto);
+        assertNotNull(uri);
+        System.out.println(uri.toString());
     }
 }
