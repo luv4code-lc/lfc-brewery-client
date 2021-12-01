d15de6a1d3494db4d5d5d3e15e1a593bc6ed7fc4
luv4code-lfc <luv4code.lc@gmail.com>
2021-12-01 13:00:56 +0530

    adding customer related crud operations getting the brewery service. and also adding the test methods for appropriate methods.

--- src/main/java/com/luv4code/lfcbreweryclient/web/client/BreweryClient.java
+++ src/main/java/com/luv4code/lfcbreweryclient/web/client/BreweryClient.java
@@ -1,6 +1,7 @@
 package com.luv4code.lfcbreweryclient.web.client;
 
 import com.luv4code.lfcbreweryclient.web.model.BeerDto;
+import com.luv4code.lfcbreweryclient.web.model.CustomerDto;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @ConfigurationProperties(value = "lfc.brewery", ignoreUnknownFields = false)
 public class BreweryClient {
     public final String BEER_PATH_V1 = "/api/v1/beer/";
+    public final String CUSTOMER_PATH_V1 = "/api/v1/customer/";
     private String apihost;
     private final RestTemplate restTemplate;
 
@@ -30,6 +32,26 @@ public class BreweryClient {
 
     public void updateBeer(UUID uuid, BeerDto beerDto) {
         restTemplate.put(apihost + BEER_PATH_V1 + "/" + uuid.toString(), beerDto);
+    }
+
+    public void deleteBeer(UUID uuid) {
+        restTemplate.delete(apihost + BEER_PATH_V1 + "/" + uuid);
+    }
+
+    public CustomerDto getCustomerById(UUID uuid) {
+        return restTemplate.getForObject(apihost + CUSTOMER_PATH_V1 + uuid.toString(), CustomerDto.class);
+    }
+
+    public URI saveNewCustomer(CustomerDto customerDto) {
+        return restTemplate.postForLocation(apihost + CUSTOMER_PATH_V1, customerDto);
+    }
+
+    public void updateCustomer(UUID uuid, CustomerDto customerDto) {
+        restTemplate.put(apihost + CUSTOMER_PATH_V1 + "/" + uuid.toString(), customerDto);
+    }
+
+    public void deleteCustomer(UUID uuid) {
+        restTemplate.delete(apihost + CUSTOMER_PATH_V1 + "/" + uuid.toString());
     }
 
     public void setApihost(String apihost) {
--- src/test/java/com/luv4code/lfcbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/com/luv4code/lfcbreweryclient/web/client/BreweryClientTest.java
@@ -1,6 +1,7 @@
 package com.luv4code.lfcbreweryclient.web.client;
 
 import com.luv4code.lfcbreweryclient.web.model.BeerDto;
+import com.luv4code.lfcbreweryclient.web.model.CustomerDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -34,5 +35,35 @@ class BreweryClientTest {
     void testUpdateBeer() {
         BeerDto beerDto = BeerDto.builder().beerName("Pale Ale").build();
         breweryClient.updateBeer(UUID.randomUUID(), beerDto);
+    }
+
+    @Test
+    void testDeleteBeer() {
+        breweryClient.deleteBeer(UUID.randomUUID());
+    }
+
+    @Test
+    void testGetBeerById() {
+        CustomerDto customerById = breweryClient.getCustomerById(UUID.randomUUID());
+        assertNotNull(customerById);
+    }
+
+    @Test
+    void testSaveNewCustomer() {
+        CustomerDto customer = CustomerDto.builder().name("Dravid").build();
+        URI uri = breweryClient.saveNewCustomer(customer);
+        assertNotNull(uri);
+        System.out.println(uri.toString());
+    }
+
+    @Test
+    void testUpdateCustomer() {
+        CustomerDto customer = CustomerDto.builder().name("Ganguly").build();
+        breweryClient.updateCustomer(UUID.randomUUID(), customer);
+    }
+
+    @Test
+    void testDeleteCustomer() {
+        breweryClient.deleteCustomer(UUID.randomUUID());
     }
 }
